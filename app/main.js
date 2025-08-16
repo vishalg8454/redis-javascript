ed3a70cec5f94a1c2383bd4026cddd5281be6e39
vishalg8454 <vishalgaurav@Vishals-MacBook-Pro.local>
2025-08-16 14:43:07 +0530

    multiple pong

--- app/main.js
+++ app/main.js
@@ -5,7 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    connection.write("+PONG\r\n");
+    console.log("Received data:", data.toString());
+  });
 });
-//
+
 server.listen(6379, "127.0.0.1");
