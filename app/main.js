64bef5c4a99cfd30e8721a3b3c01ad8b336ff27b
vishalg8454 <vishalgaurav@Vishals-MacBook-Pro.local>
2025-08-16 16:09:08 +0530

    impl GET SET

--- app/main.js
+++ app/main.js
@@ -4,6 +4,8 @@ const { rParser } = require("./parser");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const map = new Map();
+
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const str = data.toString();
@@ -16,6 +18,18 @@ const server = net.createServer((connection) => {
       if (arr[i].toLocaleUpperCase() === "ECHO") {
         const echoString = `+${arr[i + 1]}\r\n`;
         connection.write(echoString);
+      }
+      if (arr[i].toLocaleUpperCase() === "GET") {
+        const key = arr[i + 1];
+        const result = map.get(key);
+        const resultString = `${result.length}\r\n${result}\r\n`;
+        connection.write(resultString);
+      }
+      if (arr[i].toLocaleUpperCase() === "SET") {
+        const key = arr[i + 1];
+        const value = arr[i + 2];
+        map.set(key, value);
+        connection.write("+OK\r\n");
       }
     }
   });
